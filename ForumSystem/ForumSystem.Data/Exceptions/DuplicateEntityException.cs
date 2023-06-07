a1d03e3a1b520b28bcb3e46058365c51ba503c5f
Atanas Iliev <atanasiliev1293@gmail.com>
2023-06-07 15:11:13 +0300

    Added Exceptions

---
+++ ForumSystem/ForumSystem.Data/Exceptions/DuplicateEntityException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumSystem.DataAccess.Exceptions
+{
+    public class DuplicateEntityException : ApplicationException
+    {
+        public DuplicateEntityException(string message) : base(message) { }
+    }
+}
