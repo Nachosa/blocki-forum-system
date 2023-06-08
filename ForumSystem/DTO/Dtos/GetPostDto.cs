a246b3f57e17713f14df21fd145e3a99487fe48a
Atanas Iliev <atanasiliev1293@gmail.com>
2023-06-08 11:53:13 +0300

    Added DTO Assembly also disabled Nullable in all asemblies

---
+++ ForumSystem/DTO/Dtos/GetPostDto.cs
@@ -0,0 +1,27 @@
+﻿using ForumSystem.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumSystem.DataAccess.Dtos
+{
+    public class GetPostDto
+    {
+        [MinLength(16, ErrorMessage = "The {0} must be at least {1} characters long.")]
+        [MaxLength(64, ErrorMessage = "The {0} must be no more than {1} characters long.")]
+        public string Title { get; set; }
+
+        [MinLength(32, ErrorMessage = "The {0} must be at least {1} characters long.")]
+        [MaxLength(8192, ErrorMessage = "The {0} must be no more than {1} characters long.")]
+        public string Content { get; set; }
+
+        public int Likes { get; set; }
+
+        public int Dislikes { get; set; }
+
+        public ICollection<Tag> Tags { get; set; } = new HashSet<Tag>();
+    }
+}
