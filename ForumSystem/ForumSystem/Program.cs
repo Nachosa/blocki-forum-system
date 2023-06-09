d80e308026937f6770718ab0b13e0ec083a7c3eb
Nikolai Gigov <nikolaivgigov2@gmail.com>
2023-06-09 10:21:45 +0300

    -Installed JWT token package -Configured JWT token (no Issuer and Audience check) -Added hardcoded user for testing

--- ForumSystem/ForumSystem/Program.cs
+++ ForumSystem/ForumSystem/Program.cs
@@ -7,6 +7,13 @@ using ForumSystem.Business.CommentService;
 using ForumSystem.DataAccess.UserRepo;
 using ForumSystem.DataAccess.CommentRepo;
 using ForumSystem.DataAccess.PostRepo;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using ForumSystem.DataAccess.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace ForumSystemBusiness
 {
@@ -25,6 +32,27 @@ namespace ForumSystemBusiness
 
             builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
 
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    //ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    //ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey
+                    (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true
+                };
+            });
+            builder.Services.AddAuthorization();
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
@@ -36,8 +64,47 @@ namespace ForumSystemBusiness
             }
             app.UseDeveloperExceptionPage();
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.MapControllers();
-            //app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+
+            app.MapPost("/security/createToken", [AllowAnonymous] (User user) =>
+            {
+                if (user.Username == "greyWolfNinjaMaster__XX69" && user.Password == "123456789pass")
+                {
+                    //var issuer = builder.Configuration["Jwt:Issuer"];
+                    //var audience = builder.Configuration["Jwt:Audience"];
+                    var key = Encoding.ASCII.GetBytes
+                    (builder.Configuration["Jwt:Key"]);
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = new ClaimsIdentity(new[]
+                        {
+                new Claim("Id", Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+                new Claim(JwtRegisteredClaimNames.Email, user.Username),
+                new Claim(JwtRegisteredClaimNames.Jti,
+                Guid.NewGuid().ToString())
+             }),
+                        Expires = DateTime.UtcNow.AddMinutes(1),
+                        //Issuer = issuer,
+                        //Audience = audience,
+                        SigningCredentials = new SigningCredentials
+                        (new SymmetricSecurityKey(key),
+                        SecurityAlgorithms.HmacSha512Signature)
+                    };
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var token = tokenHandler.CreateToken(tokenDescriptor);
+                    var jwtToken = tokenHandler.WriteToken(token);
+                    var stringToken = tokenHandler.WriteToken(token);
+                    return Results.Ok(stringToken);
+                }
+                return Results.Unauthorized();
+            });
+
+            app.MapGet("/security/getMessage",
+() => "Hello World!").RequireAuthorization();
 
             app.Run();
         }
