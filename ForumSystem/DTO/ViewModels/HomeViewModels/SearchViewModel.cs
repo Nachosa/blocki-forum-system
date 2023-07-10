7c83d17ea0f77596a637250bf6254e748aba4979
Atanas Iliev <atanasiliev1293@gmail.com>
2023-07-10 18:51:39 +0300

    Implemented controller for Search from navbar also seeded Tags and PostTags

---
+++ ForumSystem/DTO/ViewModels/HomeViewModels/SearchViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ForumSystem.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumSystemDTO.ViewModels.HomeViewModels
+{
+	public class SearchViewModel
+	{
+		public List<Post> PostsWithTag { get; set; }
+
+		public User UserWithUsername { get; set; }
+
+		public List<Post> PostsContaingThatInput { get; set; }
+	}
+}
--- ForumSystem/ForumSystem/ViewControllers/HomeController.cs
+++ ForumSystem/ForumSystem/ViewControllers/HomeController.cs
@@ -5,6 +5,7 @@ using ForumSystem.Business.CommentService;
 using ForumSystem.Business.UserService;
 using ForumSystem.DataAccess.Exceptions;
 using ForumSystem.DataAccess.Models;
+using ForumSystem.DataAccess.QueryParams;
 using ForumSystem.DataAccess.UserRepo;
 using ForumSystemDTO.ViewModels.CommentViewModels;
 using ForumSystemDTO.ViewModels.HomeViewModels;
@@ -13,50 +14,99 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ForumSystem.Web.ViewControllers
 {
-    public class HomeController : Controller
-    {
+	public class HomeController : Controller
+	{
 		private readonly IPostService postService;
 		private readonly IUserService userService;
-        private readonly IMapper mapper;
+		private readonly IMapper mapper;
 
-        public HomeController(IPostService postService, IUserService userService, IMapper mapper)
-        {
+		public HomeController(IPostService postService, IUserService userService, IMapper mapper)
+		{
 			this.postService = postService;
 			this.userService = userService;
-            this.mapper = mapper;
-        }
+			this.mapper = mapper;
+		}
 
-        public IActionResult Index()
-        {
-            int activeUsersCount = userService.GetUsersCount();
-            int activePostsCount = postService.GetPostsCount();
+		public IActionResult Index()
+		{
+			int activeUsersCount = userService.GetUsersCount();
+			int activePostsCount = postService.GetPostsCount();
 
-            PostQueryParameters postQueryparams= new PostQueryParameters();
-            postQueryparams.SortBy = "comments";
-            postQueryparams.SortOrder = "desc";
-            List<PostViewModelAbbreviated> topCommentedPosts = postService.GetPosts(postQueryparams).Take(10).Select(p => mapper.Map<PostViewModelAbbreviated>(p)).ToList();
-            postQueryparams.SortBy = "date";
-            postQueryparams.SortOrder = "desc";
-            List<PostViewModelAbbreviated> recentlyCreatedPosts = postService.GetPosts(postQueryparams).Take(10).Select(p => mapper.Map<PostViewModelAbbreviated>(p)).ToList();
+			PostQueryParameters postQueryparams = new PostQueryParameters();
+			postQueryparams.SortBy = "comments";
+			postQueryparams.SortOrder = "desc";
+			List<PostViewModelAbbreviated> topCommentedPosts = postService.GetPosts(postQueryparams).Take(10).Select(p => mapper.Map<PostViewModelAbbreviated>(p)).ToList();
+			postQueryparams.SortBy = "date";
+			postQueryparams.SortOrder = "desc";
+			List<PostViewModelAbbreviated> recentlyCreatedPosts = postService.GetPosts(postQueryparams).Take(10).Select(p => mapper.Map<PostViewModelAbbreviated>(p)).ToList();
 
-            HomePageViewModel homePage = new HomePageViewModel();
-            homePage.UsersCount= activeUsersCount;
-            homePage.PostsCount= activePostsCount;
-            homePage.TopCommentedPosts= topCommentedPosts;
-            homePage.RecentlyCreatedPosts= recentlyCreatedPosts;
-            return View(homePage);
-        }
+			HomePageViewModel homePage = new HomePageViewModel();
+			homePage.UsersCount = activeUsersCount;
+			homePage.PostsCount = activePostsCount;
+			homePage.TopCommentedPosts = topCommentedPosts;
+			homePage.RecentlyCreatedPosts = recentlyCreatedPosts;
+			return View(homePage);
+		}
 
-        [HttpGet]
-        public IActionResult Login()
-        {
-            return RedirectToAction("Login","User");
-        }
+		[HttpGet]
+		public IActionResult Search(string input)
+		{
+			var results = new SearchViewModel();
 
-        [HttpGet]
-        public IActionResult About()
-        {
-            return View();
-        }
+			try
+			{//Searching for user with that username 
+				results.UserWithUsername = userService.GetUserByUserName(input);
+			}
+			catch (EntityNotFoundException)
+			{
+				results.UserWithUsername = null;
+			}
+			catch (Exception e)
+			{
+				this.Response.StatusCode = StatusCodes.Status500InternalServerError;
+				this.ViewData["ErrorMessage"] = e.Message;
+				return View("Error");
+			}
+
+			var postParams = new PostQueryParameters();
+
+			try
+			{//Searching for posts which contain that input in their title
+				postParams.Title = input;
+				results.PostsContaingThatInput = postService.GetPosts(postParams);
+			}
+			catch (Exception e)
+			{
+				this.Response.StatusCode = StatusCodes.Status500InternalServerError;
+				this.ViewData["ErrorMessage"] = e.Message;
+				return View("Error");
+			}
+
+			try
+			{//Searching for posts which have that input like a tag
+				postParams.Title = null;
+				postParams.Tag = input;
+				results.PostsWithTag = postService.GetPosts(postParams);
+			}
+			catch (Exception e)
+			{
+				this.Response.StatusCode = StatusCodes.Status500InternalServerError;
+				this.ViewData["ErrorMessage"] = e.Message;
+				return View("Error");
+			}
+
+			return View(results);
+		}
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return RedirectToAction("Login", "User");
+		}
+
+		[HttpGet]
+		public IActionResult About()
+		{
+			return View();
+		}
 	}
 }
