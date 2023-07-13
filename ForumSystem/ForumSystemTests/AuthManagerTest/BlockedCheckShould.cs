c49c2978486b6810344b06f60adeea613bf13c7a
moufleta <katrin.lilova@proton.me>
2023-07-13 22:02:52 +0300

    Add AuthManager unit tests

---
+++ ForumSystem/ForumSystemTests/AuthManagerTest/BlockedCheckShould.cs
@@ -0,0 +1,59 @@
+﻿using ForumSystem.Business.AuthenticationManager;
+using ForumSystem.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumSystemTests.AuthManagerTest
+{
+	[TestClass]
+	public class BlockedCheckShould
+	{
+		[TestMethod]
+		public void Return_True_When_User_Is_Blocked_By_User_Object()
+		{
+			// Arrange
+			var user = new User { RoleId = 1 };
+
+			var sut = new AuthManager(null);
+
+			// Act
+			bool result = sut.BlockedCheck(user);
+
+			// Assert
+			Assert.IsTrue(result);
+		}
+
+		[TestMethod]
+		public void Return_True_When_RoleId_Is_Blocked()
+		{
+			// Arrange
+			int roleId = 1;
+
+			var sut = new AuthManager(null);
+
+			// Act
+			bool result = sut.BlockedCheck(roleId);
+
+			// Assert
+			Assert.IsTrue(result);
+		}
+
+		[TestMethod]
+		public void Return_False_When_RoleId_Is_Not_Blocked()
+		{
+			// Arrange
+			int roleId = 2;
+
+			var sut = new AuthManager(null);
+
+			// Act
+			bool result = sut.BlockedCheck(roleId);
+
+			// Assert
+			Assert.IsFalse(result);
+		}
+	}
+}
---
+++ ForumSystem/ForumSystemTests/AuthManagerTest/UserCheckShould.cs
@@ -0,0 +1,59 @@
+﻿using ForumSystem.Business.AuthenticationManager;
+using ForumSystem.Business.UserService;
+using ForumSystem.DataAccess.Exceptions;
+using ForumSystem.DataAccess.Models;
+using ForumSystem.DataAccess.UserRepo;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumSystemTests.AuthManagerTest
+{
+	[TestClass]
+	public class UserCheckShould
+	{
+		[TestMethod]
+		public void Return_User_Successfully_When_Valid_Username_And_Password()
+		{
+			// Arrange
+			string userName = "TestUser";
+			string password = "TestPassword";
+			var encodedPassword = Convert.ToBase64String(Encoding.UTF8.GetBytes(password));
+			var user = new User { Username = userName, Password = encodedPassword };
+
+			var userServiceMock = new Mock<IUserService>();
+
+			var sut = new AuthManager(userServiceMock.Object);
+
+			userServiceMock.Setup(us => us.GetUserByUserName(userName)).Returns(user);
+
+			// Act
+			User result = sut.UserCheck(userName, password);
+
+			// Assert
+			Assert.IsNotNull(result);
+			Assert.AreEqual(user, result);
+		}
+
+		[TestMethod]
+		public void Throw_UnauthenticatedOperationException_When_Invalid_Username_Or_Password()
+		{
+			// Arrange
+			string userName = "TestUser";
+			string password = "TestPassword";
+			var user = new User { Username = userName, Password = "IncorrectPassword" };
+
+			var userServiceMock = new Mock<IUserService>();
+
+			var sut = new AuthManager(userServiceMock.Object);
+
+			userServiceMock.Setup(us => us.GetUserByUserName(userName)).Returns(user);
+
+			// Act & Assert
+			Assert.ThrowsException<UnauthenticatedOperationException>(() => sut.UserCheck(userName, password));
+		}
+	}
+}
